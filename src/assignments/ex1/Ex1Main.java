a8d6282d39190675b6f46908ab6c7678638eeb7f
Itaikami <itaikami51@gmail.com>
2024-12-10 19:37:31 +0200

    Complete

--- src/assignments/ex1/Ex1Main.java
+++ src/assignments/ex1/Ex1Main.java
@@ -17,8 +17,37 @@ public class Ex1Main {
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
             num1 = sc.next();
             if (!num1.equals("quit")) {
-                // add your code here
-
-                /////////////////////
-            }
-        }}}
+                System.out.println("num1= "+num1+" is number: "+Ex1.isNumber(num1)+" value: "+Ex1.number2Int(num1));
+                if(!Ex1.isNumber(num1))
+                {
+                    System.out.println("ERR: num1 is in the wrong format! " +"("+num1+")");
+                }
+                else
+                {
+                    System.out.println("Enter a string as number#2 (or \"quit\" to end the program):");
+                    num2= sc.next();
+                    if (!num2.equals("quit")) {
+                        System.out.println("num2= "+num2+" is number: "+Ex1.isNumber(num2)+" value: "+Ex1.number2Int(num2));
+                    }
+                    if(!Ex1.isNumber(num2))
+                    {
+                        System.out.println("ERR: num2 is in the wrong format! " +"("+num2+")");
+                    }
+                    else
+                    {
+                        System.out.println("Enter a base for output: (a number [2,16])");
+                        int base= sc.nextInt();
+                        System.out.println(num1+" + "+num2+" = "+Ex1.int2Number(Ex1.number2Int(num1)+Ex1.number2Int(num2),base));
+                        System.out.println(num1+" * "+num2+" = "+Ex1.int2Number(Ex1.number2Int(num1)*Ex1.number2Int(num2),base));
+                        String[] max=new String[4];
+                        max[0]=num1;
+                        max[1]=num2;
+                        max[2]=Ex1.int2Number(Ex1.number2Int(num1)+Ex1.number2Int(num2),base);
+                        max[3]=Ex1.int2Number(Ex1.number2Int(num1)*Ex1.number2Int(num2),base);
+                        System.out.println("Max number over ["+max[0]+','+max[1]+','+max[2]+','+max[3]+"] is: "+max[Ex1.maxIndex(max)]);
+                    }
+                }
+             }
+        }
+        System.out.println("Quitting now...");
+    }}
